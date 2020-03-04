800fb1d7a1fd8afa67f70980f8101902cb28e661
valaxy <valaxy@qq.com>
2020-03-04 21:59:04 +0800

    up

--- src/buildAdjacency.ts
+++ src/buildAdjacency.ts
@@ -18,7 +18,18 @@ const settings = [
     }
 ]
 
+
+
+// 这里修改setting
 const setting = settings[2]
+
+let targetPath = path.join(__dirname, `../data/adj/${setting.level}.json`)
+
+let adj: number[][] = []
+
+if (fs.existsSync(targetPath)) {
+    adj = fs.readJSONSync(targetPath)
+}
 
 interface Item {
     adcode: string
@@ -32,11 +43,11 @@ interface Item {
 const isAdjacentByBbox = function (a: Item, b: Item) {
     // 如果bbox不相交则跳过
     if (turf.booleanDisjoint(a.expandPolyBbox, b.expandPolyBbox)) {
-        console.log('bbox disjoint')
+        // console.log('bbox disjoint')
         return false
     }
 
-    console.log('bbox cross')
+    // console.log('bbox cross')
     for (let point of turf.coordAll(a.expandPoly)) {
         if (turf.booleanPointInPolygon(point, b.expandPoly)) {
             return true
@@ -74,7 +85,13 @@ const buildGeometry = function () {
     let level = setting.level
 
     let provinceGeos: Item[] = adcodes.map(adcode => {
-        let { multiPolygon, district } = buildGeoJson(require(`../data/geo/${level}/${adcode}.json`))
+        let raw = require(`../data/geo/${level}/${adcode}.json`)
+
+        if (raw.districts.length == 0) {
+            return null
+        }
+
+        let { multiPolygon, district } = buildGeoJson(raw)
         let scalePoly = scaleByLength(multiPolygon, 2000)
 
         // fs.outputFileSync(path.join(__dirname, `../logs/${adcode}.json`), JSON.stringify(scalePoly, null, 4))
@@ -96,16 +113,38 @@ const main = function () {
     let provinceGeos = buildGeometry()
     for (let i = 0; i < provinceGeos.length; i++) {
         let x = provinceGeos[i]
+        if (!x) { continue }
+
+        if (i in adj && adj[i].length == provinceGeos.length) {
+            continue
+        }
 
         for (let j = i + 1; j < provinceGeos.length; j++) {
             let y = provinceGeos[j]
+            if (!y) { continue }
 
-            console.log(`start calc: ${i}/${x.adcode}/${x.name} -> ${j}/${y.adcode}/${y.name}`)
+            // console.log(`start calc, ${i},${j}/${provinceGeos.length}:  ${x.adcode}/${x.name} -> ${y.adcode}/${y.name}`)
+
+            if (i in adj && j in adj[i]) {
+                continue
+            }
+
+
+            if (!(i in adj)) {
+                adj[i] = []
+            }
 
             if (isAdjacentByBbox(x, y)) {
                 console.log(`adjacent, ${x.adcode}/${x.name} -> ${y.adcode}/${y.name}`)
+
+                adj[i][j] = 1
+            } else {
+                adj[i][j] = 0
             }
         }
+
+        fs.outputFileSync(targetPath, JSON.stringify(adj))
+        console.log('write to disk')
     }
 }
 
